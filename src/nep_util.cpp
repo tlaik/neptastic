2d944bc56c7150ec3001c5bbf34e84a0be85a9cd
tlaik <tlaik92@gmail.com>
2021-07-28 15:00:44 +0300

    FPS unlock signature search

--- src/nep_util.cpp
+++ src/nep_util.cpp
@@ -5,6 +5,7 @@
 #include "nep_hooks.h"
 #include "nep_config.h"
 #include "nep_util.h"
+#include <Psapi.h>
 
 // Original bloom is done using 4 low res screenshots slapped together to produce blur.
 // It looks like shit.
@@ -35,11 +36,35 @@ void getNepWindow() {
 void fpsUnlock() {
     const int frameIntervalTarget = 10; // us
     const int ofs[2] = { 0x23A687, 0x2836A9 };
+    const uint8_t sig[] = { 0x8B, 0x7E, 0x1C, 0x85, 0xFF };
 
     // What right and proper fix doth not possess a machine code injection?
-    int32_t fpsCodeOrig = nepBase + ofs[nepGame];
-    int32_t fpsRet = fpsCodeOrig + 5;
+    int fpsCodeOrig = nepBase + ofs[nepGame];
+    int fpsRet = fpsCodeOrig + 5;
     uint8_t* hookBody = NULL;
+
+    int res = memcmp((uint8_t*)fpsCodeOrig, sig, sizeof(sig));
+    if(res != 0) {
+        NEP_LOGW("Incorrect FPS code offset (%#010x), searching by signature.\n", fpsCodeOrig);
+        fpsCodeOrig = 0;
+        MODULEINFO mi;
+        if(GetModuleInformation(GetCurrentProcess(), (HMODULE)nepBase, &mi, sizeof(mi)) == 0) {
+            NEP_LOGE("Couldn't get Nep module information, aborting FPS unlock.\n");
+            return;
+        }
+        for(int i = nepBase; i < (int)(nepBase + mi.SizeOfImage - sizeof(sig)); i++) {
+            if(memcmp((uint8_t*)i, sig, sizeof(sig)) == 0) {
+                NEP_LOGI("Found FPS code @ %#010x\n", i);
+                fpsCodeOrig = i;
+                fpsRet = fpsCodeOrig + 5;
+                break;
+            }
+        }
+        if(!fpsCodeOrig) {
+            NEP_LOGE("Couldn't find FPS code, aborting FPS unlock\n");
+            return;
+        }
+    }
 
     // Replaces: 
     // 8B 7E 1C | mov edi, [esi+1C]
@@ -54,8 +79,9 @@ void fpsUnlock() {
 
     hookBody = (uint8_t*)malloc(sizeof(hookBodyTemplate));
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++) {
         hook[1 + i] = (uint8_t)((int32_t)(hookBody - (fpsCodeOrig + 5)) >> 8 * i);
+    }
 
     for (int i = 0; i < 4; i++)
         hookBodyTemplate[1 + i] = (uint8_t)(frameIntervalTarget >> 8 * i);
@@ -65,10 +91,12 @@ void fpsUnlock() {
     memcpy(hookBody, hookBodyTemplate, sizeof(hookBodyTemplate));
 
     DWORD oldFlags;
-    VirtualProtect((void*)fpsCodeOrig, sizeof(hook), PAGE_EXECUTE_READWRITE, &oldFlags);
+    DWORD of2;
     VirtualProtect((void*)hookBody, sizeof(hookBodyTemplate), PAGE_EXECUTE_READWRITE, &oldFlags);
+    VirtualProtect((void*)fpsCodeOrig, sizeof(hook), PAGE_EXECUTE_READWRITE, &oldFlags);
     // FPS go brrrr
     memcpy((void*)fpsCodeOrig, hook, sizeof(hook));
+    VirtualProtect((void*)fpsCodeOrig, sizeof(hook), oldFlags, &of2);
 
     NEP_LOGI("FPS unlocked\n")
 }
